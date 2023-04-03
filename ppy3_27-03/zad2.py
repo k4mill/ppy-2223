d896360218e7efe01d84ac88eb8ca9f4d2478bca
Kamil Pławiak <kamil@plawiak.com>
2023-04-03 18:08:13 +0200

    fix zad2 to accept multiple arguments instead of 1

--- ppy3_27-03/zad2.py
+++ ppy3_27-03/zad2.py
@@ -1,20 +1,22 @@
 import math
 
-def prime(number):
-    is_prime = True
-    for i in range(2, int(math.sqrt(number)) + 1):
-        if number % i == 0:
+def prime(*numbers):
+    for number in numbers:
+        is_prime = True
+
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+        if number < 2:
             is_prime = False
-            break
 
-    if number < 2:
-        is_prime = False
+        if is_prime:
+            print(f"{number} is prime")
+        else:
+            print(f"{number} is not prime")
 
-    if is_prime:
-        print(f"{number} is prime")
-    else:
-        print(f"{number} is not prime")
+
 
 if __name__ == '__main__':
-    for i in range(0, 100):
-        prime(i)
+    prime(1,2,3,4,5,6,7,8,9,10,11,12,13,15,17,18,19,23,24,25,29,30)
